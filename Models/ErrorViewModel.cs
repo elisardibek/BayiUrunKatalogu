52610276f01f42ab62583800f2a9a55b9a69dc8c
Yazilim STAJYER <yazilimstajyer@index.com.tr>
2026-09-11 10:38:32 +0300

    Initial commit: ASP.NET MVC proje iskeleti

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BayiUrunKatalogu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
